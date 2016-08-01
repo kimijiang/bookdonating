7efb3ea4ee11d95e2e612ca4f15182de7aa76c46
kimijiang <1017938873@qq.com>
2016-08-01 20:05:07 +0800

    navbar and books

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Project
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.i18n.default_locale = :cn  
+    config.i18n.default_locale = :en 
 
   end
 end
