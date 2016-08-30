8ccdac798e26e35b0ba62a8472ac54728487986f
dounan <dounan1@gmail.com>
2016-08-31 03:17:53 +0800

    added views to theme

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :books
   devise_for :users
-  root 'pages#home'
+  root 'books#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
 
   # Example of regular route:
     get "about" => "pages#about"
+    get "contact" => "pages#contact"
+    get "pricing" => "pages#pricing"
+    get "index" => "books#index"
+
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
