d45af2a2b4bb6893561e437a88377bb5548f0247
kimijiang <1017938873@qq.com>
2016-09-02 22:16:28 +0800

    add name age and gender to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160901132422) do
+ActiveRecord::Schema.define(version: 20160902140209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,10 @@ ActiveRecord::Schema.define(version: 20160901132422) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "home_address"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "age"
+    t.string   "gender"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
